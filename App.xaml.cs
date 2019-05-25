f9c21c7af91eb97e61194c35be4c29e0ae31b84c
kiba518 <liujin_hzzh@126.com>
2019-05-25 19:09:38 +0800

    Wpf的全局异常事件记录
    Wpf的全局异常事件记录

---
+++ App.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using Utility;
+
+namespace PurchasePlatform
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    { 
+        protected override void OnLoadCompleted(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnLoadCompleted(e);
+            Current.DispatcherUnhandledException += App_OnDispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        } 
+
+        /// <summary>
+        /// UI线程抛出全局异常事件处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                OliveLogger.Error("UI线程全局异常", e.Exception);
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                OliveLogger.Error("不可恢复的UI线程全局异常", ex);
+            }
+        }
+
+        /// <summary>
+        /// 非UI线程抛出全局异常事件处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                var exception = e.ExceptionObject as Exception;
+                if (exception != null)
+                {
+                    OliveLogger.Error("非UI线程全局异常", exception);
+                }
+            }
+            catch (Exception ex)
+            {
+                OliveLogger.Error("不可恢复的非UI线程全局异常", ex);
+
+            }
+        }
+
+    }
+}
