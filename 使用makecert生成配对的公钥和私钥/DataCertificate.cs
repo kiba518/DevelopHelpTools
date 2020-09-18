9bc24f20607ffcf241f8b23ffae07e2af52c0ffb
liujin <liujin_hzzh@126.com>
2020-09-18 17:03:25 +0800

    添加文件
    添加文件

---
+++ 使用makecert生成配对的公钥和私钥/DataCertificate.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanPeng.Desktop.Licence
+{
+    public sealed class DataCertificate
+    {
+        #region 生成证书 
+        /// <summary> 
+        /// 根据指定的证书名和makecert全路径生成证书（包含公钥和私钥，并保存在MY存储区） 
+        /// </summary> 
+        /// <param name="subjectName"></param> 
+        /// <param name="makecertPath"></param> 
+        /// <returns></returns> 
+        public static bool CreateCertWithPrivateKey(string subjectName, string makecertPath)
+        {
+            subjectName = "CN=" + subjectName;
+            string param = " -pe -ss my -n \"" + subjectName + "\" ";
+            try
+            {
+                Process p = Process.Start(makecertPath, param);
+                p.WaitForExit();
+                p.Close();
+            }
+            catch (Exception e)
+            {
+               
+                return false;
+            }
+            return true;
+        }
+        #endregion
+
+        #region 文件导入导出 
+        /// <summary> 
+        /// 从WINDOWS证书存储区的个人MY区找到主题为subjectName的证书， 
+        /// 并导出为pfx文件，同时为其指定一个密码 
+        /// 并将证书从个人区删除(如果isDelFromstor为true) 
+        /// </summary> 
+        /// <param name="subjectName">证书主题，不包含CN=</param> 
+        /// <param name="pfxFileName">pfx文件名</param> 
+        /// <param name="password">pfx文件密码</param> 
+        /// <param name="isDelFromStore">是否从存储区删除</param> 
+        /// <returns></returns> 
+        public static bool ExportToPfxFile(string subjectName, string pfxFileName,
+            string password, bool isDelFromStore)
+        {
+            subjectName = "CN=" + subjectName;
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            X509Certificate2Collection storecollection = (X509Certificate2Collection)store.Certificates;
+            foreach (X509Certificate2 x509 in storecollection)
+            {
+                if (x509.Subject == subjectName)
+                {
+                     
+
+                    byte[] pfxByte = x509.Export(X509ContentType.Pfx, password);
+                    using (FileStream fileStream = new FileStream(pfxFileName, FileMode.Create))
+                    {
+                        // Write the data to the file, byte by byte. 
+                        for (int i = 0; i < pfxByte.Length; i++)
+                            fileStream.WriteByte(pfxByte[i]);
+                        // Set the stream position to the beginning of the file. 
+                        fileStream.Seek(0, SeekOrigin.Begin);
+                        // Read and verify the data. 
+                        for (int i = 0; i < fileStream.Length; i++)
+                        {
+                            if (pfxByte[i] != fileStream.ReadByte())
+                            {
+                                 
+                                fileStream.Close();
+                                return false;
+                            }
+                        }
+                        fileStream.Close();
+                    }
+                    if (isDelFromStore == true)
+                        store.Remove(x509);
+                }
+            }
+            store.Close();
+            store = null;
+            storecollection = null;
+            return true;
+        }
+        /// <summary> 
+        /// 从WINDOWS证书存储区的个人MY区找到主题为subjectName的证书， 
+        /// 并导出为CER文件（即，只含公钥的） 
+        /// </summary> 
+        /// <param name="subjectName"></param> 
+        /// <param name="cerFileName"></param> 
+        /// <returns></returns> 
+        public static bool ExportToCerFile(string subjectName, string cerFileName)
+        {
+            subjectName = "CN=" + subjectName;
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            X509Certificate2Collection storecollection = (X509Certificate2Collection)store.Certificates;
+            foreach (X509Certificate2 x509 in storecollection)
+            {
+                if (x509.Subject == subjectName)
+                {
+                    
+                    //byte[] pfxByte = x509.Export(X509ContentType.Pfx, password); 
+                    byte[] cerByte = x509.Export(X509ContentType.Cert);
+                    using (FileStream fileStream = new FileStream(cerFileName, FileMode.Create))
+                    {
+                        // Write the data to the file, byte by byte. 
+                        for (int i = 0; i < cerByte.Length; i++)
+                            fileStream.WriteByte(cerByte[i]);
+                        // Set the stream position to the beginning of the file. 
+                        fileStream.Seek(0, SeekOrigin.Begin);
+                        // Read and verify the data. 
+                        for (int i = 0; i < fileStream.Length; i++)
+                        {
+                            if (cerByte[i] != fileStream.ReadByte())
+                            {
+                                
+                                fileStream.Close();
+                                return false;
+                            }
+                        }
+                        fileStream.Close();
+                    }
+                }
+            }
+            store.Close();
+            store = null;
+            storecollection = null;
+            return true;
+        }
+        #endregion
+
+        #region 从证书中获取信息 
+        /// <summary> 
+        /// 根据私钥证书得到证书实体，得到实体后可以根据其公钥和私钥进行加解密 
+        /// 加解密函数使用DEncrypt的RSACryption类 
+        /// </summary> 
+        /// <param name="pfxFileName"></param> 
+        /// <param name="password"></param> 
+        /// <returns></returns> 
+        public static X509Certificate2 GetCertificateFromPfxFile(string pfxFileName, string password)
+        {
+            try
+            {
+                return new X509Certificate2(pfxFileName, password, X509KeyStorageFlags.Exportable);
+            }
+            catch (Exception e)
+            { 
+                   
+                return null;
+            }
+        }
+        /// <summary> 
+        /// 到存储区获取证书 
+        /// </summary> 
+        /// <param name="subjectName"></param> 
+        /// <returns></returns> 
+        public static X509Certificate2 GetCertificateFromStore(string subjectName)
+        {
+            subjectName = "CN=" + subjectName;
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            X509Certificate2Collection storecollection = (X509Certificate2Collection)store.Certificates;
+            foreach (X509Certificate2 x509 in storecollection)
+            {
+                if (x509.Subject == subjectName)
+                {
+                    return x509;
+                }
+            }
+            store.Close();
+            store = null;
+            storecollection = null;
+            return null;
+        }
+        /// <summary> 
+        /// 根据公钥证书，返回证书实体 
+        /// </summary> 
+        /// <param name="cerPath"></param> 
+        public static X509Certificate2 GetCertFromCerFile(string cerPath)
+        {
+            try
+            {
+                return new X509Certificate2(cerPath);
+            }
+            catch (Exception e)
+            {
+               
+                return null;
+            }
+        }
+        #endregion
+    }
+}
---
+++ 使用makecert生成配对的公钥和私钥/DataCertificateHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Utility;
+namespace VanPeng.Desktop.Licence
+{
+    public  class DataCertificateHelper
+    { 
+        public static  void CopyPfxAndGetInfo()
+        { 
+            string keyName = "VanPeng.Desktop.Licence";
+            var ret = DataCertificate.CreateCertWithPrivateKey(keyName, @"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\makecert.exe");
+            if(ret)
+            {
+                DataCertificate.ExportToPfxFile(keyName, "VanPeng.pfx", "123456", true);
+                X509Certificate2 x509 = DataCertificate.GetCertificateFromPfxFile("VanPeng.pfx", "123456");
+                string publickey = x509.PublicKey.Key.ToXmlString(false);
+                string privatekey = x509.PrivateKey.ToXmlString(true);
+
+                string myname = "my name is VanPeng.Desktop.Licence!";
+                string enStr = RSAEncrypt(publickey, myname);
+                MessageBox.Show("密文是：" + enStr);
+                string deStr = RSADecrypt(privatekey, enStr);
+                MessageBox.Show("明文是：" + deStr);
+            }
+        }
+       
+      
+       
+        /// <summary> 
+        /// RSA解密 
+        /// </summary> 
+        /// <param name="xmlPrivateKey"></param> 
+        /// <param name="m_strDecryptString"></param> 
+        /// <returns></returns> 
+        public static string RSADecrypt(string xmlPrivateKey, string m_strDecryptString)
+        {
+            RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
+            provider.FromXmlString(xmlPrivateKey);
+            byte[] rgb = Convert.FromBase64String(m_strDecryptString);
+            byte[] bytes = provider.Decrypt(rgb, false);
+            return new UnicodeEncoding().GetString(bytes);
+        }
+        /// <summary> 
+        /// RSA加密 
+        /// </summary> 
+        /// <param name="xmlPublicKey"></param> 
+        /// <param name="m_strEncryptString"></param> 
+        /// <returns></returns> 
+        public static string RSAEncrypt(string xmlPublicKey, string m_strEncryptString)
+        {
+            RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
+            provider.FromXmlString(xmlPublicKey);
+            byte[] bytes = new UnicodeEncoding().GetBytes(m_strEncryptString);
+            return Convert.ToBase64String(provider.Encrypt(bytes, false));
+        }
+       
+      
+    }
+}
